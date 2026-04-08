68bdc8e91ac6633e71ba6fbd1653085772d90f13
hardge33 <cansizaraba@gmail.com>
2026-04-08 13:49:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def generate():
             '-loop', '1',
             '-i', img_tmp.name,
             '-vf', vf,
-            '-t', '8',
+            '-t', '5',
             '-pix_fmt', 'yuv420p',
             '-c:v', 'libx264',
             output_path
